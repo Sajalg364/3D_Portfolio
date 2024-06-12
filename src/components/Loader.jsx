226dc006779dafa6bc8cfc69c51bdcd6d58af0d9
Sajalg364 <sajalmahajan364@gmail.com>
2024-06-13 03:21:07 +0530

    going for sleep

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,9 +1,9 @@
-import { HTML , useProgress } from '@react-three/drei';
+import {Html, useProgress} from '@react-three/drei';
 
 const Loader = () => {
   const {progress} = useProgress();
   return (
-    <HTML>
+    <Html>
       <span className='canvas-load'></span>
       <p style={{
         fontSize: 14,
@@ -11,7 +11,7 @@ const Loader = () => {
         color: '#f1f1f1',
         marginTop: 40
       }}>{progress.toFixed(2)}%</p>
-    </HTML>
+    </Html>
   )
 }
 
--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -2,7 +2,7 @@ import { Suspense, useEffect, useState } from "react";
 import { Canvas } from "@react-three/fiber";
 import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
 
-// import CanvasLoader from "../Loader"
+import CanvasLoader from "../Loader"
 import Loader from "../Loader";
 
 const Computers = ({isMobile}) => {
