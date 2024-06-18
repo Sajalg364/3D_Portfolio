9b3b3ae2d4329a2083f66fc4e2948a8ed1dfb570
Sajalg364 <sajalmahajan364@gmail.com>
2024-06-18 10:16:07 +0530

    some optimization

--- src/components/index.js
+++ src/components/index.js
@@ -2,9 +2,7 @@ import { EarthCanvas, BallCanvas, ComputersCanvas, StarsCanvas } from './canvas'
 import Hero from './Hero';
 import Navbar from './Navbar';
 import About from './About';
-import Experience from './Experience';
 import Works from './Works';
-import Feedbacks from './Feedbacks';
 import Contact from './Contact';
 import Education from './Education';
 import Footer from './Footer';
@@ -13,9 +11,7 @@ export {
   Hero,
   Navbar,
   About,
-  Experience,
   Works,
-  Feedbacks,
   Contact,
   EarthCanvas, 
   BallCanvas, 
